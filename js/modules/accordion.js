0b0acfa3836fdb84b02b1767c877a052ea24199e
Eduardo Brito <eduardobritodev@gmail.com>
2023-11-17 15:15:24 -0300

    refatoracao do accordion

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,18 +1,32 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
-  const activeClass = 'ativo';
-
-  function activeAccordion() {
-    this.classList.toggle(activeClass);
-    this.nextElementSibling.classList.toggle(activeClass);
+export default class Accordion {
+  constructor(elem, name) {
+    this.accordionlist = document.querySelectorAll(elem);
+    this.ativoClass = name;
+    this.activeaccordion = this.activeaccordion.bind(this);
+    console.log(this.activeaccordion);
   }
 
-  if (accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
+  activeaccordion(event) {
+    event.target.classList.toggle(this.ativoClass);
+    event.target.nextElementSibling.classList.toggle(this.ativoClass);
+  }
 
-    accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
+  addevent() {
+    this.accordionlist.forEach((item) => {
+      item.addEventListener("click", this.activeaccordion);
     });
+  }
+
+  check() {
+    if (this.accordionlist.length) {
+      this.accordionlist[0].classList.add(this.ativoClass);
+      this.accordionlist[0].nextElementSibling.classList.add(this.ativoClass);
+    }
+  }
+
+  init2() {
+    this.addevent();
+    this.check();
+    return this;
   }
 }
