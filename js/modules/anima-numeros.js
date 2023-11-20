e8abbcb2d9a5780486d8c7ce9547776557b77e90
Eduardo Brito <eduardobritodev@gmail.com>
2023-11-20 18:09:59 -0300

    fetchAnimais-refatorado

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -25,9 +25,7 @@ export default class AnimaNumeros {
   }
 
   animaNumeros() {
-    this.numeros.forEach((numero) =>
-      this.constructor.incrementarNumero(numero)
-    );
+    this.numeros.forEach(numero => this.constructor.incrementarNumero(numero));
   }
   //ocorre quando a mutacao ocorre
 
