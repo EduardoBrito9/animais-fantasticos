b4af0b4644b8d1a1c2aca9162135229ecca64b84
Eduardo Brito <eduardobritodev@gmail.com>
2023-11-20 18:34:09 -0300

    fetchBitcoin refatorado

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ import Tooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
-import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import FetchBitcoin from "./modules/fetch-bitcoin.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 
@@ -38,5 +38,5 @@ initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
 
-initFetchBitcoin();
+FetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
 initAnimacaoScroll();
